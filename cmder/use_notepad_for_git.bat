e46495b78423f03b6fbcdc6752832ba8c00fe46d
Richard Vuong <richard.vuong@rakuten.com>
2018-07-31 13:47:38 +0100

    updating use n++ with git

--- cmder/use_notepad_for_git.bat
+++ cmder/use_notepad_for_git.bat
@@ -1,1 +1,1 @@
-FOR /F "usebackq tokens=2*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\App Paths\notepad++.exe" /ve`) DO git config --global core.editor "'%B' -multiInst -notabbar -nosession -noPlugin"
+git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
